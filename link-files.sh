7b9aabb387d4b3c15f772a8dea5fb4711009307a
Gavrie Philipson <gavriep@il.ibm.com>
2014-12-15 18:42:14 +0200

    updates

---
+++ link-files.sh
@@ -0,0 +1,7 @@
+ln -sf config/.bashrc ~
+ln -sf config/.gitconfig ~
+ln -sf config/.gitignore ~
+ln -sf config/.inputrc ~
+ln -sf config/.pythonrc.py ~
+ln -sf config/.tmux.conf ~
+ln -sf config/.zshrc ~
